4444607ac6d2b54e7a7ea35ac3302e7a2a10af1d
lanemaster <admin@getalane.com>
2023-07-13 13:39:01 -0700

    Updates for tank

--- src/app/tank3d/tank3d.component.ts
+++ src/app/tank3d/tank3d.component.ts
@@ -4,6 +4,7 @@ import * as THREE from 'three';
 import { GLTFLoader, GLTF } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { CSS2DRenderer } from 'three/examples/jsm/renderers/CSS2DRenderer.js';
+import { DatahubService } from '../datahub.service';
 
 
 @Component({
@@ -28,6 +29,7 @@ export class Tank3dComponent implements AfterViewInit {
   private light3!: THREE.PointLight;
   private light4!: THREE.PointLight;
   private tank: any;
+  private ground: any;
   private liquid: any;
   private directionalLight!: THREE.DirectionalLight;
 
@@ -38,7 +40,15 @@ export class Tank3dComponent implements AfterViewInit {
   private loaderGLTF = new GLTFLoader();
   private renderer!: THREE.WebGLRenderer;
   private scene!: THREE.Scene;
-  constructor() { }
+  constructor(private datahub: DatahubService) {
+    datahub.getData('TankID').subscribe(data => {
+      const level = parseFloat(data[0].value);
+      if (this.liquid) {
+        this.liquid.scale.z = level / 100;
+        this.liquid.position.z = -1.3 + level / 100;
+      }
+      });
+  }
 
   ngAfterViewInit() {
     this.createScene();
@@ -53,18 +63,6 @@ export class Tank3dComponent implements AfterViewInit {
     }
   }
 
-  private animatetank() {
-    if (this.liquid) {
-      if(this.liquid.scale.z < 1) {
-        this.liquid.scale.z += 0.005;
-        this.liquid.position.z -= 0.005;
-      } else {
-        this.liquid.scale.z = 0;
-        this.liquid.position.z = 0;
-      }
-    }
-  }
-
   private createControls = () => {
     const renderer = new CSS2DRenderer();
     renderer.setSize(this.canvas.width, this.canvas.height);
@@ -72,8 +70,8 @@ export class Tank3dComponent implements AfterViewInit {
     renderer.domElement.style.top = '0px';
     document.body.appendChild(renderer.domElement);
     this.controls = new OrbitControls(this.camera, renderer.domElement);
-    this.controls.autoRotate = true;
-    this.controls.enableZoom = true;
+    this.controls.enableRotate = false;
+    this.controls.enableZoom = false;
     this.controls.enablePan = false;
     this.controls.update();
   };
@@ -82,19 +80,27 @@ export class Tank3dComponent implements AfterViewInit {
     //? Scene
     this.scene = new THREE.Scene();
     this.scene.background = new THREE.Color(0xffffff)
-    this.loaderGLTF.load('assets/tank.glb', (gltf: GLTF) => {
+    this.loaderGLTF.load('assets/water_tank.glb', (gltf: GLTF) => {
+      this.tank = gltf.scene.children[0];
+      var box = new THREE.Box3().setFromObject(this.tank);
+      box.getCenter(this.tank.position); 
+      this.tank.position.multiplyScalar(-1);
+      this.tank.material.color.setHex(0xdddddee);
+      this.scene.add(this.tank);
+      this.ground = gltf.scene.children[0];
+      var box = new THREE.Box3().setFromObject(this.ground);
+      box.getCenter(this.ground.position); 
+      this.ground.position.multiplyScalar(-1);
+      this.ground.material.color.setHex(0x005500);
+      this.scene.add(this.ground);
       this.liquid = gltf.scene.children[0];
       console.log(this.liquid);
       var box = new THREE.Box3().setFromObject(this.liquid);
       box.getCenter(this.liquid.position); 
       this.liquid.position.multiplyScalar(-1);
+      this.liquid.material.color.setHex(0x000055);
       this.scene.add(this.liquid);
-      this.tank = gltf.scene.children[0];
-      var box = new THREE.Box3().setFromObject(this.tank);
-      box.getCenter(this.tank.position); 
-      this.tank.position.multiplyScalar(-1);
-      this.scene.add(this.tank);
-   });
+    });
 
    //? Camera
    let aspectRatio = this.getAspectRatio();
@@ -104,9 +110,11 @@ export class Tank3dComponent implements AfterViewInit {
      this.nearClippingPane,
      this.farClippingPane
    )
-   this.camera.position.x = 100;
-   this.camera.position.y = 100;
+   this.camera.position.x = 0;
+   this.camera.position.y = -310;
    this.camera.position.z = 100;
+
+   const lightColor = 0x555555;
    this.ambientLight = new THREE.AmbientLight(0x00000, 100);
    this.scene.add(this.ambientLight);
    this.directionalLight = new THREE.DirectionalLight(0xffdf04, 0.4);
@@ -114,16 +122,16 @@ export class Tank3dComponent implements AfterViewInit {
    this.directionalLight.castShadow = true
 
    this.scene.add(this.directionalLight);
-   this.light1 = new THREE.PointLight(0x4b371c, 10);
+   this.light1 = new THREE.PointLight(lightColor, 10);
    this.light1.position.set(0, 200, 400);
    this.scene.add(this.light1);
-   this.light2 = new THREE.PointLight(0x4b371c, 10);
+   this.light2 = new THREE.PointLight(lightColor, 10);
    this.light2.position.set(500, 100, 0);
    this.scene.add(this.light2);
-   this.light3 = new THREE.PointLight(0x4b371c, 10);
-   this.light3.position.set(0, 100, -500);
+   this.light3 = new THREE.PointLight(lightColor, 10);
+   this.light3.position.set(0, -300, -500);
    this.scene.add(this.light3);
-   this.light4 = new THREE.PointLight(0x4b371c, 10);
+   this.light4 = new THREE.PointLight(lightColor, 10);
    this.light4.position.set(-500, 300, 500);
    this.scene.add(this.light4);
   }
@@ -141,7 +149,6 @@ export class Tank3dComponent implements AfterViewInit {
     let component: Tank3dComponent = this;
     (function render() {
       component.renderer.render(component.scene, component.camera);
-      // component.animatetank();
       requestAnimationFrame(render);
     }());
   }
