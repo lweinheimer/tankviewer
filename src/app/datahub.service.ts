b307bd4e9af4911dfef0cb6f01d9d0b9795fb1e6
lanemaster <admin@getalane.com>
2023-07-13 10:29:46 -0700

    Changes

--- src/app/datahub.service.ts
+++ src/app/datahub.service.ts
@@ -1,9 +1,18 @@
+
 import { Injectable } from '@angular/core';
+
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DatahubService {
-
-  constructor() { }
+  baseURL: string = 'https://fakedh.azurewebsites.net/api/v1';
+ 
+  constructor(private http: HttpClient) {}
+ 
+  getData(assetId: string): Observable<any> {
+    return this.http.get(this.baseURL + '/data' + '?asset=' + assetId);
+  }
 }
